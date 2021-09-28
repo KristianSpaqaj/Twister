95b39d338dd63e7671b3afc8c95f24dea11f54cb
KristianSpaqaj <kris49j7@edu.easj.dk>
2021-09-28 13:41:34 +0200

    Initial commit

---
+++ app/src/main/java/repository/AuthAppRepository.kt
@@ -0,0 +1,39 @@
+package repository
+
+import androidx.lifecycle.MutableLiveData
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+
+class AuthAppRepository {
+    private lateinit var auth: FirebaseAuth
+    val userLiveData: MutableLiveData<FirebaseUser> = MutableLiveData<FirebaseUser>()
+    val loggedOutData: MutableLiveData<Boolean> = MutableLiveData<Boolean>()
+    val errorMessage: MutableLiveData<String> = MutableLiveData<String>()
+
+    init {
+        userLiveData.value = FirebaseAuth.getInstance().currentUser
+    }
+    fun Register(email: String,password: String) {
+        auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener { task ->
+            if (task.isSuccessful){
+                userLiveData.value = FirebaseAuth.getInstance().currentUser
+            } else {
+                errorMessage.value = task.exception?.message
+            }
+        }
+    }
+    fun signIn(email: String,password: String){
+        auth.signInWithEmailAndPassword(email, password).addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                userLiveData.value = FirebaseAuth.getInstance().currentUser
+            } else {
+                errorMessage.value = task.exception?.message
+            }
+        }
+    }
+    fun signOut(){
+        auth.signOut()
+        loggedOutData.value = true
+    }
+
+}
